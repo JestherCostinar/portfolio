b83fb4f48edfd9d385687e8a3a31085d7a37cf10
Jesther Costinar <jesther.jc15@gmail.com>
2023-03-09 18:22:37 +0800

    first commit

---
+++ assets/projects.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    title: "Kasect",
+    description:
+      "A website wherein you can showcase your project here in Kasect. This system is made in laravel, alphine js and tailwind, it contain routing, eloquent, validation, form request and authentication.",
+    image: "assets/kasect.png",
+    github: "https://github.com/rakeshsangem/harkfolio",
+    live: "https://sharkfolio.vercel.app/",
+  },
+  {
+    title: "Kartmir",
+    description:
+      "Kasmir Cart is an e-commerce website built on some of the most popular open source technologies such as Codeigniter, Vanilla JS and Bootstrap.",
+    image: "assets/kasect.png",
+    github: "https://github.com/rakeshsangem/bankist-application",
+    live: "https://bankist-application.vercel.app",
+  },
+  {
+    title: "AccuraFind",
+    description:
+      "A website that advertises a job, suggests proficient candidates that are suited for specific company.",
+    image: "assets/accurafind.png",
+    github: "https://github.com/rakeshsangem/portflio",
+    live: "https://www.rakeshsangem.me",
+  },
+  
+];
